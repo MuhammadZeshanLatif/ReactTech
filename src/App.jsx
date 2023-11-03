deb89aca5eb6ca6983e056fcfbbf89d4ff1a3bbe
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-11-03 12:47:17 +0500

    UPdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import './App.css';
+import { useState } from 'react';
 function App() {
   return (
     <>
-      <div>
-        Hello world
-      </div>
+      <Card></Card>
     </>
-  );
+  )
 }
+
+
+//Component
+function Card() {
+  let [heading,getHeading] = useState(0);
+  //function
+  function check() {
+    getHeading(++heading);
+  }
+  
+  return (
+    <>
+      <h1 id="heading" onClick={check}>{heading}</h1>
+    </>
+  )
+}
+
+
 export default App;
